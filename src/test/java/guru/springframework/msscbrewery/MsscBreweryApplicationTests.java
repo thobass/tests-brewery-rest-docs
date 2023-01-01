5377a85307e71dbba9a75b4d72fa27579530a9b1
Thomas Basset <thomas@basset.rocks>
2023-01-01 19:52:34 +0100

    Assigment review + initial code

---
+++ src/test/java/guru/springframework/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbrewery;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
